9f3fb46c6df57c99c299cdc8a73bcff0bf2250ef
faisal <faisal@gmail.com>
2022-05-24 05:44:33 -0700

    enable the mobile filter on mobile view and page

--- app/views.py
+++ app/views.py
@@ -42,8 +42,12 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request, data=None):
+    if data == None:
+        mobile = Product.objects.filter(category='M')
+    elif data == 'Sumsing' or data == 'Nokia':
+        mobile = Product.objects.filter(category='M').filter(brand=data)
+    return render(request, 'app/mobile.html', {'mobile':mobile})
 
 def login(request):
  return render(request, 'app/login.html')
