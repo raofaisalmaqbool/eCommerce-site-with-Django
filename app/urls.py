5d9295f206fac0d77c4b5e741097a6b03503e82c
faisal <faisal@gmail.com>
2022-05-27 04:32:40 -0700

    change password configrations

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
-from .forms import LoginForm
+from .forms import LoginForm, MyPasswordChangeForm
 
 
 urlpatterns = [
@@ -16,13 +16,14 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
+    # path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     # path('login/', views.login, name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     # path('registration/', views.customerregistration, name='customerregistration'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm), name='passwordchange'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
