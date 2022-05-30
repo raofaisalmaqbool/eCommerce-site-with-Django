54de67468412ded56a032d84b33e5f11fab5d672
faisal <faisal@gmail.com>
2022-05-29 23:51:26 -0700

    compleate athanicatin work

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UsernameField, AuthenticationForm, PasswordResetForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UsernameField, AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth import password_validation 
@@ -28,6 +28,11 @@ class MyPasswordChangeForm(PasswordChangeForm):
 
 class MyPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(label=_("Email"), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email', 'class':'form-control'}))
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomlete':'new-password', 'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(label=_("Confirm Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomlete':'new-password', 'class':'form-control'}))
 
 
 
--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from app import views
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
-from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
 
 
 urlpatterns = [
@@ -22,11 +22,14 @@ urlpatterns = [
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     # path('login/', views.login, name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     # path('registration/', views.customerregistration, name='customerregistration'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), 
     path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
     path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name='app/profile.html'), name='passwordchangedone'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
-    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/customerregistration.html'), name='password_reset_complete'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
