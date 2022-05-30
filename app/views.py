10aeb6ff6b7703ca62ac2772bd0671cbca3cd00f
faisal <faisal@gmail.com>
2022-05-30 05:26:16 -0700

    apply the logic for total amount show

--- app/views.py
+++ app/views.py
@@ -38,7 +38,19 @@ def show_cart(request):
     if request.user.is_authenticated:
         user = request.user
         cart = Cart.objects.filter(user=user)
-        return render(request, 'app/addtocart.html', {'carts':cart})
+
+        amount = 0.0
+        shipping_amount = 70.0
+        total_amount = 0.0
+        cart_product = [p for p in Cart.objects.all() if p.user==user]
+
+        if cart_product:
+            for p in cart_product:
+                tempamount = (p.quantity * p.product.discounted_price)
+                amount += tempamount
+                total_amount = amount + shipping_amount
+
+        return render(request, 'app/addtocart.html', {'carts':cart, 'total_amount':total_amount, 'amount':amount})
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
