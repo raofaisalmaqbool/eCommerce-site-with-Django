0927757ef4cbe0a302b71239ee49c01e30118cfd
faisal <faisal@gmail.com>
2022-06-03 04:19:55 -0700

    paypal pament integration successful

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import (Customer, Product, Cart, OrderPlaced)
 
-@admin.register(Customer)
-class CustomerModelAdmin(admin.ModelAdmin):
+@admin.register(Customer)      
+class CustomerModelAdmin(admin.ModelAdmin):# register modeld admin class by decorator
     list_display = ['id', 'user', 'name', 'locality', 'city', 'zipcode', 'state']
 
 @admin.register(Product)
@@ -15,4 +17,12 @@ class CartModelAdmin(admin.ModelAdmin):
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']
+    list_display = ['id', 'user', 'customer', 'customer_info', 'product', 'product_info', 'quantity', 'ordered_date', 'status']
+
+    def customer_info(self, obj):
+        link = reverse("admin:app_customer_change", args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.customer.name)
+    
+    def product_info(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
